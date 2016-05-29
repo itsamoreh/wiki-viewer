e147ab4d7de11b488a909484837c3bd65fa6c671
itsamoreh <itsamoreh@gmail.com>
2016-05-29 13:55:56 -0400

    finishing touches

--- js/wiki-viewer.js
+++ js/wiki-viewer.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   $(".searchString").keydown(function(e) {
     // if enter is pressed in the search input
     if(e.keyCode === 13) {
@@ -9,18 +8,13 @@ $(document).ready(function() {
   });
 });
 
-var jsonDATA;
-
 function search () {
   var searchString = ($(".searchString").val());
-
   $.ajax( {
     url: "//en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchString + "&limit=20",
     dataType: "jsonp",
     success: function(data) {
-
       $(".resultsBox").html("");
-
       for (var x = 0; x < data[0].length; x++) {
         $(".resultsBox").append(  '<div class="large-12 columns result"><div class="callout"><h2 class="resultTitle"><a href=' +
                                   data[3][x] +
@@ -31,9 +25,7 @@ function search () {
                                   data[2][x] +
                                   '</p></div></div>'
                                );
-
       }
-
     }
   });
 }
