3cc6b8b304356a0b333be45d6e450ed0aa0ba9d3
itsamoreh <itsamoreh@gmail.com>
2016-05-29 23:46:32 -0400

    fixed undefined search results

--- js/wiki-viewer.js
+++ js/wiki-viewer.js
@@ -7,15 +7,16 @@ $(document).ready(function() {
     }
   });
 });
-
+var json;
 function search () {
   var searchString = ($(".searchString").val());
   $.ajax( {
     url: "//en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchString + "&limit=20",
     dataType: "jsonp",
     success: function(data) {
+      json = data;
       $(".resultsBox").html("");
-      for (var x = 0; x < data[0].length; x++) {
+      for (var x = 0; x < data[1].length; x++) {
         $(".resultsBox").append(  '<div class="large-12 columns result"><div class="callout"><h2 class="resultTitle"><a href=' +
                                   data[3][x] +
                                   ' class="resultLink" target="_blank">' +
